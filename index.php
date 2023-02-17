4354b4fcb71b19459d59fa3fc9231713aa74ded0
Arslan Ahsan <arslanahsanjalib050@gmail.com>
2023-02-17 11:44:35 +0500

    code added

---
+++ index.php
@@ -0,0 +1,12 @@
+ <textarea readonly class="my_txt">
+At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+                   At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+                   At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+                   At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+                   At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+                   At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+                   At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+                   At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+                   At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+                   At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+</textarea>
---
+++ script.js
@@ -0,0 +1,3 @@
+var a=document.getElementsByClassName("my_txt");
+height1=a[0].scrollHeight;
+a[0].style.height=height1 + "px";
